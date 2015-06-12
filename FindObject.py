937350081c4996a7e52864b25b89f84c3e67e17c
David Fridovich-Keil <david.fridovichkeil@gmail.com>
2015-06-12 12:13:30 -0400

    edited README

---
+++ FindObject.py
@@ -0,0 +1,7 @@
+"""
+Provides a function to detect an object in an image, given training images
+which show the object (and masks to identify specifically which pixels in
+those training images constitute the object in question).
+"""
+
+
